f1230afa64dafb2f8bbc5af9a7d01ae2d39e59d9
Matt Torres <matttorres@Matts-MacBook-Air.local>
2018-07-13 12:20:24 -0400

    Working with database

--- main.go
+++ main.go
@@ -3,46 +3,79 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 	"errors"
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
 )
+
+type Team struct {
+	ID int `json:"id" sql:"id"`
+	Name string `json:"name" sql:"name"`
+	Manager string `json:"manager" sql:"manager"`
+	Stadium string `json:"stadium" sql:"stadium"`
+	City string `json:"city" sql:"city"`
+	State string `json:"state" sql:"state"`
+}
 
 var (
 	cubs    = make(map[string]string)
 	marlins = make(map[string]string)
+	db            *sql.DB
+	mysqlHost     string = "localhost"
+	mysqlDatabase string = "mlb_team_stats"
+	mysqlUser     string = "root"
+	mysqlPassword string = "pa11word"
+	dbInfo        string = mysqlUser + ":" + mysqlPassword + "@tcp(" + mysqlHost + ":3306)/" + mysqlDatabase + "?charset=utf8"
 )
 
 func main() {
+	var rows *sql.Rows
+	var err error
 	team := os.Args[1:]
 
-	// Chicago Cubs
-	cubs["stadium"] = "Wrigley"
-	cubs["manager"] = "Maddon"
-	cubs["location"] = "Chicago, Illinois"
-	cubs["team average"] = strconv.Itoa(266)
-	cubs["hits"] = strconv.Itoa(792)
-
-	// Miami Marlins
-	marlins["stadium"] = "Marlins Park"
-	marlins["manager"] = "Mattingly"
-	marlins["location"] = "Miami, Florida"
-	marlins["average"] = strconv.Itoa(242)
-	marlins["hits"] = strconv.Itoa(751)
-
-	// printTeamStats using the getTeam function
-	data,err := getTeam(team[0])
+	// Connect to database
+	db, err = sql.Open("mysql", dbInfo)
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		// Check for second paramater
-		if (len(team) == 2) { // ==2 so we have a second paramater
-			printTeamStats(data,team[1])
-		} else {
-			printTeamStats(data,"")
-		}
-
+		fmt.Printf("ERR: %s - %q", "Cannot connect to database", err)
 	}
+	defer db.Close()
+
+	// Get all assets
+	rows, err = db.Query("SELECT * FROM teams WHERE name = (?)",team[0])
+
+	// Check to make sure there were no errors in querying the data
+	if err != nil {
+		// Could not query the db, send empty response and error.
+		fmt.Printf("ERROR: %q",err)
+	}
+
+	// Defer the cosing of the db connection
+	defer rows.Close()
+
+	// Iterate through the rows and add them to the assets.
+	for rows.Next() {
+		t := Team{}
+		// Scan the results into the assets variable type Assets
+		err := rows.Scan(&t.ID, &t.Name, &t.Manager, &t.Stadium, &t.City, &t.State)
+
+		// Check for error during scan
+		if err != nil {
+
+			// Could not scan query results into assets, retun empty van and error
+			fmt.Printf("ERROR: %q",err)
+
+		}
+	fmt.Println(t)
+	}
+
+
+
+
+
+
+
+
 
 }
 
